999abfee646ba1c34289c3fecb48c3510944f8a3
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2024-07-17 14:01:08 -0300

    part 2 completed

--- 04-pizzashop-web/src/routes.tsx
+++ 04-pizzashop-web/src/routes.tsx
@@ -5,6 +5,7 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
+import { SignUp } from './pages/auth/sign-up'
 
 export const router: Router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ export const router: Router = createBrowserRouter([
       {
         path: '/sign-in',
         element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
       },
     ],
   },
