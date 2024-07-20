313f93ca6938d00e6f9cc98f4122078690e267ef
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2024-07-20 14:48:09 -0300

    integrando frontend backend part 3

--- 04-pizzashop-web/src/routes.tsx
+++ 04-pizzashop-web/src/routes.tsx
@@ -3,7 +3,9 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { Dashboard } from './pages/app/dashboard'
+import { NotFound } from './pages/404'
+import { Dashboard } from './pages/app/dashboard/dashboard'
+import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
 
@@ -11,10 +13,15 @@ export const router: Router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: '/',
         element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
       },
     ],
   },
