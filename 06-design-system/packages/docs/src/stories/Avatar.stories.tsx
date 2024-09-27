0c9138f6f124df431348a5617fb362372f77c03e
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2024-09-27 09:07:23 -0300

    design system module 4 finished

--- 06-design-system/packages/docs/src/stories/Avatar.stories.tsx
+++ 06-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/lucas-ferrari-correa.png',
     alt: 'Lucas Ferrari',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- 06-design-system/packages/docs/src/stories/Text.stories.tsx
+++ 06-design-system/packages/docs/src/stories/Text.stories.tsx
@@ -8,6 +8,28 @@ export default {
     children:
       'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo fugiat tenetur ullam! Commodi, voluptates ea? Esse, error! A quasi fugiat, expedita id, mollitia doloribus sint, voluptatibus harum voluptatum ad illum?',
   },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
+  },
 } as Meta<TextProps>
 
 export const Primary: StoryObj<TextProps> = {}
