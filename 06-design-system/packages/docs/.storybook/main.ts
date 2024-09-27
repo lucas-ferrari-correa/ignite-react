bb4a06b2f25b2109a00a79cd752295131585cfff
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2024-09-27 10:05:40 -0300

    ci: storybook deploy fix path

--- 06-design-system/packages/docs/.storybook/main.ts
+++ 06-design-system/packages/docs/.storybook/main.ts
@@ -16,7 +16,7 @@ const config: StorybookConfig = {
   },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
-      config.base = '/05-design-system/'
+      config.base = '/06-design-system/'
     }
     return config
   },
