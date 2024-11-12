6cae9361bd02727e502f787cf38511eec1d4c27c
Lucas Ferrari <lucasfcorrea2206@gmail.com>
2024-11-12 07:07:59 -0300

    ignite call 8 finished

---
+++ 07-ignite-call/src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -0,0 +1,8 @@
+// import { CalendarStep } from './CalendarStep'
+import { ConfirmStep } from './ConfirmStep'
+
+export function ScheduleForm() {
+  // return <CalendarStep />
+
+  return <ConfirmStep />
+}
